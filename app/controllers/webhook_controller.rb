8747a639ec15aa5811d895a273d96457292895a7
shota sato <last11142@gmail.com>
2023-12-20 16:48:26 +0900

    refactor: 不要な空白を削除

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -32,7 +32,7 @@ class WebhookController < ApplicationController
             type: 'text',
             text: @response_text
           }
-          client.reply_message(event['replyToken'], message)          
+          client.reply_message(event['replyToken'], message)
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = client.get_message_content(event.message['id'])
           tf = Tempfile.open("content")
